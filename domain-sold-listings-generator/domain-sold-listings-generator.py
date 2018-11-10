725267a233ba5343356cc89ad75c6ef43ea541ec
stewart leach <stew.leach@gmail.com>
2018-11-10 14:47:31 +1100

    WIP connecting to local database. Need to update connect method to include hostname etc

--- domain-sold-listings-generator/domain-sold-listings-generator.py
+++ domain-sold-listings-generator/domain-sold-listings-generator.py
@@ -1,2 +1,10 @@
+import psycopg2
+
+conn = psycopg2.connect("dbname=property-dashboard-dev user=postgres")
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+
 my_string = "Hello, World!"
 print(my_string)
