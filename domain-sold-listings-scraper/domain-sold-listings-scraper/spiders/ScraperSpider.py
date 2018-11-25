14ef952bf3d88e060259c1e99b3d5fc1e7b4d827
stewart leach <stew.leach@gmail.com>
2018-11-25 14:10:12 +1100

    parsing scraped page and extracting data

--- domain-sold-listings-scraper/domain-sold-listings-scraper/spiders/ScraperSpider.py
+++ domain-sold-listings-scraper/domain-sold-listings-scraper/spiders/ScraperSpider.py
@@ -1,6 +1,6 @@
 import scrapy
 
-
+#building type is part of the url query parameter. Run as separate jobs and add type as a db column
 class DomainSoldListingSpider(scrapy.Spider):
     name = "domain_soldlisting_spider"
     start_urls = ['https://www.domain.com.au/sold-listings/?postcode=3000']
@@ -10,12 +10,28 @@ class DomainSoldListingSpider(scrapy.Spider):
         for resultset in response.css(RESULTS_SELECTOR):
 
             NAME = './/@data-reactid'
-            STREET_ADDRESS = './/div/div[2]/div[2]/a/h2/span[1]/text()[1]'
             SOLD_PRICE = './/div/div[2]/div[1]/p/text()'
+            SOLD_DATE_TYPE = './/div/div[1]/span/text()'  # need to split result into seperate db cols
+            STREET_ADDRESS = './/div/div[2]/div[2]/a/h2/span[1]/text()[1]'
+            CITY = './/div/div[2]/div[2]/a/h2/span[2]/span[1]/text()'
+            STATE = './/div/div[2]/div[2]/a/h2/span[2]/span[2]/text()'
+            POSTCODE = './/div/div[2]/div[2]/a/h2/span[2]/span[3]/text()'
+            NUMBER_BEDROOMS = './/div/div[2]/div[3]/div/span[1]/span/text()'
+            NUMBER_BATHROOMS = './/div/div[2]/div[3]/div/span[2]/span/text()'
+            NUMBER_CARPARKS = './/div/div[2]/div[3]/div/span[3]/span/text()'
+            SQUARE_METERS = './/div/div[2]/div[3]/div/span[4]/span/text()'  # extract integer value from result 52 m2
             yield {
                 'name': resultset.xpath(NAME).extract_first(),
                 'sold-price': resultset.xpath(SOLD_PRICE).extract_first(),
+                'sold-date-type': resultset.xpath(SOLD_DATE_TYPE).extract_first(),
                 'street-address': resultset.xpath(STREET_ADDRESS).extract_first(),
+                'city': resultset.xpath(CITY).extract_first(),
+                'state': resultset.xpath(STATE).extract_first(),
+                'postcode': resultset.xpath(POSTCODE).extract_first(),
+                'number-bedrooms': resultset.xpath(NUMBER_BEDROOMS).extract_first(),
+                'number-bathrooms': resultset.xpath(NUMBER_BATHROOMS).extract_first(),
+                'number-carparks': resultset.xpath(NUMBER_CARPARKS).extract_first(),
+                'square-meters': resultset.xpath(SQUARE_METERS).extract_first(),
             }
 
          #print(resultset)
